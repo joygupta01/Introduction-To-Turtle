285f6be1c178d44d2ee93b067a85fe80bfe8755c
joygupta01 <joygupta052@gmail.com>
2025-06-02 21:25:06 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.Screen().bgcolor("yellow")
+turtle.Screen().setup(400,400)
+turtle.title("Turtle")
+board=turtle.Turtle()
+for i in range (4):
+    board.forward(100)
+    board.left(90)
+turtle.done()
+    
